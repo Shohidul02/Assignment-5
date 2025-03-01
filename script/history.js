fff757183f3f880bb47b5dcded4d3379db0e5485
Shohidul02 <shohidul.arif02@gmail.com>
2025-03-01 18:18:16 +0600

    card section text ok

--- script/history.js
+++ script/history.js
@@ -22,15 +22,16 @@ document.getElementById("card-2").addEventListener('click',
     function(event){
         event.preventDefault();
         const historyContainer=document.getElementById("history-container");
-        let title1 = document.getElementById("title-1").innerText;
+        let title2 = document.getElementById("title-2").innerText;
         let now = new Date();
+        let dateFormate = today.toDateString();
         let hours = now.getHours();
         let minutes = now.getMinutes();
         let amPm = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12; // Convert to 12-hour format
         const p=document.createElement("p");
             p.innerText = `
-            You have completed the task ${title1} at ${now} ${hours}:${minutes} ${amPm}
+            You have completed the task ${title2} at ${dateFormate} ${hours}:${minutes} ${amPm}
             `
             historyContainer.appendChild(p);
 
@@ -41,15 +42,16 @@ document.getElementById("card-3").addEventListener('click',
     function(event){
         event.preventDefault();
         const historyContainer=document.getElementById("history-container");
-        let title1 = document.getElementById("title-1").innerText;
+        let title3 = document.getElementById("title-3").innerText;
         let now = new Date();
+        let dateFormate = today.toDateString();
         let hours = now.getHours();
         let minutes = now.getMinutes();
         let amPm = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12; // Convert to 12-hour format
         const p=document.createElement("p");
             p.innerText = `
-            You have completed the task ${title1} at ${now} ${hours}:${minutes} ${amPm}
+            You have completed the task ${title3} at ${dateFormate} ${hours}:${minutes} ${amPm}
             `
             historyContainer.appendChild(p);
 
@@ -60,15 +62,16 @@ document.getElementById("card-4").addEventListener('click',
     function(event){
         event.preventDefault();
         const historyContainer=document.getElementById("history-container");
-        let title1 = document.getElementById("title-1").innerText;
+        let title4 = document.getElementById("title-4").innerText;
         let now = new Date();
+        let dateFormate = today.toDateString();
         let hours = now.getHours();
         let minutes = now.getMinutes();
         let amPm = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12; // Convert to 12-hour format
         const p=document.createElement("p");
             p.innerText = `
-            You have completed the task ${title1} at ${now} ${hours}:${minutes} ${amPm}
+            You have completed the task ${title4} at ${dateFormate} ${hours}:${minutes} ${amPm}
             `
             historyContainer.appendChild(p);
 
@@ -79,15 +82,16 @@ document.getElementById("card-5").addEventListener('click',
     function(event){
         event.preventDefault();
         const historyContainer=document.getElementById("history-container");
-        let title1 = document.getElementById("title-1").innerText;
+        let title5 = document.getElementById("title-5").innerText;
         let now = new Date();
+        let dateFormate = today.toDateString();
         let hours = now.getHours();
         let minutes = now.getMinutes();
         let amPm = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12; // Convert to 12-hour format
         const p=document.createElement("p");
             p.innerText = `
-            You have completed the task ${title1} at ${now} ${hours}:${minutes} ${amPm}
+            You have completed the task ${title5} at ${dateFormate} ${hours}:${minutes} ${amPm}
             `
             historyContainer.appendChild(p);
 
@@ -98,15 +102,16 @@ document.getElementById("card-6").addEventListener('click',
     function(event){
         event.preventDefault();
         const historyContainer=document.getElementById("history-container");
-        let title1 = document.getElementById("title-1").innerText;
+        let title6 = document.getElementById("title-6").innerText;
         let now = new Date();
+        let dateFormate = today.toDateString();
         let hours = now.getHours();
         let minutes = now.getMinutes();
         let amPm = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12; // Convert to 12-hour format
         const p=document.createElement("p");
             p.innerText = `
-            You have completed the task ${title1} at ${now} ${hours}:${minutes} ${amPm}
+            You have completed the task ${title6} at ${dateFormate} ${hours}:${minutes} ${amPm}
             `
             historyContainer.appendChild(p);
 
